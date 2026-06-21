982a05f7e49d5832cf4868d37444c8483dc2d949
zhijie435 <wushanxi@gmail.com>
2026-06-22 06:04:39 +0800

    .3999348844658896:a8430764bfa40a3c87b5c64937886630_6a385f4eefe0da7731bae1b9.6a385f57efe0da7731bae1bb.6a385f57b43c361b680e2df4:Trae CN.T(2026/6/22 06:01:59)

--- backend/src/Exception/WalletException.php
+++ backend/src/Exception/WalletException.php
@@ -4,4 +4,49 @@ namespace Dealer\Wallet\Exception;
 
 class WalletException extends \RuntimeException
 {
+    private array $rollbackInfo = [];
+
+    private array $retryInfo = [];
+
+    public function setRollbackInfo(array $info): self
+    {
+        $this->rollbackInfo = $info;
+        return $this;
+    }
+
+    public function getRollbackInfo(): array
+    {
+        return $this->rollbackInfo;
+    }
+
+    public function setRetryInfo(array $info): self
+    {
+        $this->retryInfo = $info;
+        return $this;
+    }
+
+    public function getRetryInfo(): array
+    {
+        return $this->retryInfo;
+    }
+
+    public function hasRollbackInfo(): bool
+    {
+        return !empty($this->rollbackInfo);
+    }
+
+    public function hasRetryInfo(): bool
+    {
+        return !empty($this->retryInfo);
+    }
+
+    public function getFullContext(): array
+    {
+        return [
+            'message' => $this->getMessage(),
+            'code' => $this->getCode(),
+            'rollback' => $this->rollbackInfo,
+            'retry' => $this->retryInfo,
+        ];
+    }
 }
